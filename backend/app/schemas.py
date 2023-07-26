ac4f69f2b49f382a752c8e41ee608b374aef7ead
Brandon Goddard <brandonkgoddard@gmail.com>
2023-07-26 00:14:22 -0500

    Add elastic mappings

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -65,3 +65,49 @@ class AllAggs(TotalAggs, TopAggs):
     pass
 
     
+
+ELASTIC_MAP = {
+    "properties": {
+        "type": {"type": "byte"},
+        "title": {"type": "text"},
+        "director": {"type": "keyword"},
+        "cast": {
+            "type": "keyword",
+            "fields": {
+                "raw": {
+                    "type": "keyword"}
+            }
+        },
+        "country": {"type": "keyword"},
+        "date_added": {"type": "date",
+                       "format": "MMddyyyy"},
+        "release_year": {"type": "integer"},
+        "rating": {"type": "byte"},
+        "duration": {"type": "integer"},
+        "genres": {
+            "type": "keyword",
+            "fields": {
+                "raw": {
+                    "type": "keyword"}
+                }
+            },
+        "description": {"type": "text"}}
+}
+
+RATING_TO_ID = {
+    'PG-13': 0,
+    'TV-MA': 1,
+    'PG': 2,
+    'TV-14': 3,
+    'TV-PG': 4,
+    'TV-Y': 5,
+    'TV-Y7': 6,
+    'R': 7,
+    'TV-G': 8,
+    'G': 9,
+    'NC-17': 10,
+    'NR': 11,
+    'TV-Y7-FV': 12,
+    'UR': 13
+}
+ID_TO_RATING = {value: key for key, value in RATING_TO_ID.items()}
