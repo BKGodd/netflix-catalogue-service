e5c209e2813182a548e5acd68e8b4e6817fca61d
Brandon Goddard <brandonkgoddard@gmail.com>
2023-07-27 08:34:32 -0500

    Complete app component unit testing

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,29 +1,75 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+@Component({
+  selector: 'app-search',
+  template: 'App Search Component',
+})
+class AppSearchComponent {}
+
+@Component({
+  selector: 'app-aggs',
+  template: 'App Aggs Component',
+})
+class AppAggsComponent {}
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, AppSearchComponent, AppAggsComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
-  it(`should have as title 'frontend_angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend_angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend_angular app is running!');
+  });
+
+  it('should create the app component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize showSearchContent to true and showAggsContent to false', () => {
+    expect(component.showSearchContent).toBe(true);
+    expect(component.showAggsContent).toBe(false);
+  });
+
+  it('should set showSearchContent to true and showAggsContent to false when calling loadSearchContent()', () => {
+    component.loadSearchContent();
+
+    expect(component.showSearchContent).toBe(true);
+    expect(component.showAggsContent).toBe(false);
+  });
+
+  it('should set showAggsContent to true and showSearchContent to false when calling loadAggsContent()', () => {
+    component.loadAggsContent();
+
+    expect(component.showAggsContent).toBe(true);
+    expect(component.showSearchContent).toBe(false);
+  });
+
+  it('should display the "app-search" component when showSearchContent is true', () => {
+    component.loadSearchContent();
+    fixture.detectChanges();
+    const appSearchElement = fixture.nativeElement.querySelector('app-search');
+    const appAggsElement = fixture.nativeElement.querySelector('app-aggs');
+    expect(appSearchElement).toBeTruthy();
+    expect(appAggsElement).toBeFalsy();
+  });
+  
+  it('should display the "app-aggs" component when showAggsContent is true', () => {
+    component.loadAggsContent();
+    fixture.detectChanges();
+    const appAggsElement = fixture.nativeElement.querySelector('app-aggs');
+    const appSearchElement = fixture.nativeElement.querySelector('app-search');
+    expect(appAggsElement).toBeTruthy();
+    expect(appSearchElement).toBeFalsy();
   });
 });
